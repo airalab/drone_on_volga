c06f88308ee4b7d9f625014a7e9c86f4e0d51a60
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-04-24 20:29:01 +0400

    GPS data logger

--- waspmote/pixhawk_to_storage.py
+++ waspmote/pixhawk_to_storage.py
@@ -3,17 +3,27 @@
 
 import sys
 import serial
+import time
 from datetime import datetime
+from pymavlink import mavutil
 
 
 try:
     device = sys.argv[1]
 except IndexError:
     device = '/dev/ttyUSB0'
-print('pixhawk read and store starting with ' + device + '...')
-pixhawk = serial.Serial(device, 115200)
-try:
-    data = pixhawk.readline()
-    print(data)
-except:
-    print('reading error')
+print('pixhawk starting with ' + device + '...')
+px = mavutil.mavlink_connection(device, 57600)
+px.wait_heartbeat()
+px.mav.request_data_stream_send(px.target_system, px.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1)
+while True:
+    try:
+        msg = px.recv_match(blocking=True)
+        if msg.get_type() == 'GLOBAL_POSITION_INT':
+            print(str(datetime.now()) + ' ' + str(msg))
+            with open('/root/px.log', 'a+') as log:
+                log.write(str(datetime.now().timestamp()) + ' ' + str(msg))
+    except KeyboardInterrupt:
+        exit(0)
+    except Exception as e:
+        print('reading error: ', e)
--- waspmote/waspmote_to_storage.py
+++ waspmote/waspmote_to_storage.py
@@ -3,6 +3,7 @@
 
 import sys
 import serial
+import time
 from datetime import datetime
 
 
@@ -10,12 +11,15 @@ try:
     device = sys.argv[1]
 except IndexError:
     device = '/dev/ttyUSB0'
-print('starting with ' + device + '...')
+print('waspmote starting with ' + device + '...')
 waspmote = serial.Serial(device, 115200)
-try:
-    data = waspmote.readline().decode('ascii', 'backslashreplace') # read '\n' terminated line
-    print(data)
-    with open('/root/waspmote.log', 'a+') as log:
-        log.write(str(datetime.now().timestamp()) + ' ' + data)
-except:
-    print('reading error')
+while True:
+    try:
+        data = waspmote.readline().decode('ascii', 'backslashreplace') # read '\n' terminated line
+        print(str(datetime.now()) + ' ' + data, end='')
+        with open('/root/waspmote.log', 'a+') as log:
+            log.write(str(datetime.now().timestamp()) + ' ' + data)
+    except KeyboardInterrupt:
+        exit(0)
+    except Exception as e:
+        print('reading error: ', e)
